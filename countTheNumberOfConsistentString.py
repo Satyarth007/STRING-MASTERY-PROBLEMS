aa378dca0150caf3d218a98ad2205f51b0afe2c9
SATYARTH <satyarthtiwari123@gmail.com>
2023-02-28 01:37:28 +0530

    LEETCODE 1684

--- countTheNumberOfConsistentString.py
+++ countTheNumberOfConsistentString.py
@@ -2,7 +2,7 @@ class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         al=list(allowed)
         count=0
-        for i in words:
+        for i in words: 
             sum=0
             for j in al:
                 sum+= i.count(j)
