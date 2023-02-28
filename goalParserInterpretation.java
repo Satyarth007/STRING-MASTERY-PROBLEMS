9d949ed6141eee92af4f7223de9656b96123cdfa
SATYARTH <satyarthtiwari123@gmail.com>
2023-02-28 17:06:55 +0530

    LEETCODE 1678

---
+++ goalParserInterpretation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder s=new StringBuilder();
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i)=='G') s.append("G");
+            else{
+                if(command.charAt(i+1)==')'){
+                    s.append("o");
+                    i++;
+                }
+                else{
+                    s.append("al");
+                    i=i+3;
+                }
+            }
+        }
+        return s.toString();
+        
+    }
+}
