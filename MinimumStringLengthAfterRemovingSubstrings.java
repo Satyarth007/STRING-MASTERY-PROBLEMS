f912945770c482e9b944fd980374b1837e6875ff
SATYARTH <satyarthtiwari123@gmail.com>
2023-08-19 19:27:18 +0530

    LEETCODE 2969

---
+++ MinimumStringLengthAfterRemovingSubstrings.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minLength(String s) {
+        if(!s.contains("AB") && !s.contains("CD")) return s.length();
+
+        return minLength(s.replace("AB","").replace("CD",""));
+        
+    }
+}
